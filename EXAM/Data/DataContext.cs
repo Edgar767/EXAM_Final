917fa97f26e735a10cd03d8216be704dd24c363a
edgar <utm20040079@utma.edu.mx>
2022-12-12 15:43:22 -0600

    Archivos Data

---
+++ EXAM/Data/DataContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EXAM.Models;
+
+namespace EXAM.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext (DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EXAM.Models.User> User { get; set; } = default!;
+
+        public DbSet<EXAM.Models.Category> Category { get; set; }
+
+        public DbSet<EXAM.Models.Question> Question { get; set; }
+
+        public DbSet<EXAM.Models.QuestionAnswer> QuestionAnswer { get; set; }
+
+        public DbSet<EXAM.Models.Role> Role { get; set; }
+
+        public DbSet<EXAM.Models.Survey> Survey { get; set; }
+
+        public DbSet<EXAM.Models.UserAnswer> UserAnswer { get; set; }
+    }
+}
